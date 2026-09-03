b68319273c38b89659944e5cb34fa8c06310de2e
Zheng, Lei <realthunder.dev@gmail.com>
2026-09-03 22:41:56 +0800

    Editor: Don't duplicate the float * ImVec2 operator ImGui defines since r19270
    Dear ImGui 1.92.7 added operator*(const float, const ImVec2&) to its
    IMGUI_DEFINE_MATH_OPERATORS set, so imgui_extra_math redefining it is
    a compile error against that version and newer. Guard it the way the
    == / != and unary - operators already are.

--- imgui_extra_math.h
+++ imgui_extra_math.h
@@ -34,7 +34,9 @@ struct ImLine
 inline bool operator==(const ImVec2& lhs, const ImVec2& rhs);
 inline bool operator!=(const ImVec2& lhs, const ImVec2& rhs);
 # endif
+# if IMGUI_VERSION_NUM < 19270
 inline ImVec2 operator*(const float lhs, const ImVec2& rhs);
+# endif
 # if IMGUI_VERSION_NUM < 18955
 inline ImVec2 operator-(const ImVec2& lhs);
 # endif
